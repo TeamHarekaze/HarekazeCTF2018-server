2bbd55622538b5f3c7707b8ddce41a21b1fcc5fd
HayatoDoi <b1517914@planet.kanazawa-it.ac.jp>
2017-12-29 20:32:50 +0900

    fix bug💢

--- src/migrate.sql
+++ src/migrate.sql
@@ -1,9 +1,9 @@
 -- user
-CREATE TABLE user (
+CREATE TABLE `user` (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(40) UNIQUE NOT NULL,
     `email` VARCHAR(40) UNIQUE NOT NULL,
-    `enable` boolean DEFAULT ture,
+    `enable` boolean DEFAULT true,
     `hashed_password` VARCHAR(512),
     `team_id` INT(60),
     `forget_hash` VARCHAR(512) DEFAULT NULL,
@@ -11,16 +11,16 @@ CREATE TABLE user (
 )DEFAULT CHARSET=utf8;
 
 -- team
-CREATE TABLE team (
+CREATE TABLE `team` (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(40) UNIQUE NOT NULL,
-    `enable` boolean DEFAULT ture,
+    `enable` boolean DEFAULT true,
     `is_admin` boolean DEFAULT false,
     `hashed_password` VARCHAR(256)
 )DEFAULT CHARSET=utf8;
 
 -- question
-CREATE TABLE question (
+CREATE TABLE `question` (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(40) UNIQUE NOT NULL,
     `flag` VARCHAR(40),
@@ -28,7 +28,7 @@ CREATE TABLE question (
     `sentence` TEXT
 )DEFAULT CHARSET=utf8;
 
-CREATE TABLE answer (
+CREATE TABLE `answer` (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `user_id` INT(60),
     `question_id` INT(60),
@@ -36,18 +36,18 @@ CREATE TABLE answer (
     `create_time` DATE NOT NULL
 )DEFAULT CHARSET=utf8;
 
-CREATE TABLE genre_type (
+CREATE TABLE `genre_type` (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(40) UNIQUE NOT NULL
 )DEFAULT CHARSET=utf8;
 
-CREATE TABLE question_genre (
+CREATE TABLE `question_genre` (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `question_id` INT(60),
     `genre_type_id` INT(60)
 )DEFAULT CHARSET=utf8;
 
-CREATE TABLE question_file (
+CREATE TABLE `question_file` (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `question_id` INT(60),
     `name` VARCHAR(40),

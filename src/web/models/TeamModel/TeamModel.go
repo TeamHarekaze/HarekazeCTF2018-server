a37638b5ea6e9f382cce31dd7c33d6dab7543659
HayatoDoi <b1517914@planet.kanazawa-it.ac.jp>
2017-12-29 15:19:09 +0900

    can create and join in teams.

--- src/web/models/TeamModel/TeamModel.go
+++ src/web/models/TeamModel/TeamModel.go
@@ -103,6 +103,69 @@ func (m *TeamModel) Disable(id int) error {
 	}
 	return nil
 }
+func (m *TeamModel) PasswordCheck(name string, password string) (bool, error) {
+	m.Open()
+	defer m.Close()
+
+	hashedPassword := GenerateHashedPassword(password)
+	stmtOut, err := m.Connection.Prepare(fmt.Sprintf("SELECT COUNT(name) FROM %s WHERE name = ? AND hashed_password = ?", m.Table))
+	if err != nil {
+		return false, err
+	}
+
+	var count int
+	if err := stmtOut.QueryRow(name, hashedPassword).Scan(&count); err != nil {
+		return false, err
+	}
+	return count == 1, nil
+}
+
+func (m *TeamModel) UsedChack(name string) (bool, error) {
+	m.Open()
+	defer m.Close()
+
+	stmtOut, err := m.Connection.Prepare(fmt.Sprintf("SELECT COUNT(name) FROM %s WHERE name = ?", m.Table))
+	if err != nil {
+		return false, err
+	}
+
+	var count int
+	if err := stmtOut.QueryRow(name).Scan(&count); err != nil {
+		return false, err
+	}
+	return count != 0, nil
+}
+func (m *TeamModel) Add(name string, password string) error {
+	m.Open()
+	defer m.Close()
+
+	hashedPassword := GenerateHashedPassword(password)
+	stmtOut, err := m.Connection.Prepare(fmt.Sprintf("INSERT INTO %s (name, hashed_password) VALUES(?, ?)", m.Table))
+	if err != nil {
+		return errors.New("Database : query error")
+	}
+	if stmtOut.QueryRow(name, hashedPassword) == nil {
+		fmt.Println(err)
+		return errors.New("Database error")
+	}
+	return nil
+}
+
+func (m *TeamModel) Join(teamName string, userName string) error {
+	m.Open()
+	defer m.Close()
+
+	query := "INSERT INTO team_member (team_id, user_id) SELECT id ,(SELECT id FROM team WHERE name = ?) FROM user WHERE name = ?"
+	stmtOut, err := m.Connection.Prepare(query)
+	if err != nil {
+		return errors.New("Database : query error")
+	}
+	if stmtOut.QueryRow(teamName, userName) == nil {
+		fmt.Println(err)
+		return errors.New("Database error")
+	}
+	return nil
+}
 
 // func (m *TeamModel) FindMember(id int) ([]string, error) {
 // 	m.Open()
@@ -121,23 +184,6 @@ func (m *TeamModel) Disable(id int) error {
 // 		teams = append(teams, team)
 // 	}
 // 	return teams, nil
-// }
-
-// func (m *UserModel) Add(name string, email string, password string) error {
-// 	m.Open()
-// 	defer m.Close()
-
-// 	hashedPassword := GenerateHashedPassword(password)
-// 	stmtOut, err := m.Connection.Prepare(fmt.Sprintf("INSERT INTO %s (name, email, hashed_password) VALUES(?, ?, ?)", m.Table))
-// 	if err != nil {
-// 		return errors.New("Database : query error")
-// 	}
-// 	// error is here...why err == nil
-// 	if err := stmtOut.QueryRow(name, email, hashedPassword); err == nil {
-// 		fmt.Println(err)
-// 		return errors.New("Database error")
-// 	}
-// 	return nil
 // }
 
 // func (m *UserModel) PasswordCheck(email string, password string) (string, error) {
@@ -183,25 +229,4 @@ func (m *TeamModel) Disable(id int) error {
 // 		return errors.New("Database error")
 // 	}
 // 	return nil
-// }
-
-// func (m *UserModel) UsedChack(name string, email string) (bool, error) {
-// 	m.Open()
-// 	defer m.Close()
-
-// 	var users []User
-
-// 	query := fmt.Sprintf("select id, name, email, enable from %s WHERE name = ? OR email = ?", m.Table)
-// 	rows, err := m.Connection.Query(query, name, email)
-// 	if err != nil {
-// 		return false, errors.New("Database : query error")
-// 	}
-// 	for rows.Next() {
-// 		var user User
-// 		if err := rows.Scan(&user.id, &user.name, &user.email, &user.enable); err != nil {
-// 			return false, errors.New("Database error")
-// 		}
-// 		users = append(users, user)
-// 	}
-// 	return len(users) != 0, nil
 // }

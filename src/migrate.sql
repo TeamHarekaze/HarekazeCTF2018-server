36f879013cc282967e24d8ea834d6662ad9f1a90
HayatoDoi <b1517914@planet.kanazawa-it.ac.jp>
2017-12-29 20:03:55 +0900

    change DB design close #32

--- src/migrate.sql
+++ src/migrate.sql
@@ -3,36 +3,20 @@ CREATE TABLE user (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(40) UNIQUE NOT NULL,
     `email` VARCHAR(40) UNIQUE NOT NULL,
-    `enable` INT(1) DEFAULT '0',
-    `hashed_password` VARCHAR(512)
-)DEFAULT CHARSET=utf8;
-
-CREATE TABLE user_forget (
-    `id` INT(60)PRIMARY KEY AUTO_INCREMENT,
-    `user_id` INT(60),
-    `url_hash` VARCHAR(256),
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)DEFAULT CHARSET=utf8;
-
-CREATE TABLE user_register (
-    `id` INT(60)PRIMARY KEY AUTO_INCREMENT,
-    `user_id` INT(60),
-    `url_hash` VARCHAR(256),
-    `create_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    `enable` boolean DEFAULT ture,
+    `hashed_password` VARCHAR(512),
+    `team_id` INT(60),
+    `forget_hash` VARCHAR(512) DEFAULT NULL,
+    `register_hash` VARCHAR(512) DEFAULT NULL
 )DEFAULT CHARSET=utf8;
 
 -- team
 CREATE TABLE team (
     `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(40) UNIQUE NOT NULL,
-    `enable` INT(1) DEFAULT '1',
+    `enable` boolean DEFAULT ture,
+    `is_admin` boolean DEFAULT false,
     `hashed_password` VARCHAR(256)
-)DEFAULT CHARSET=utf8;
-
-CREATE TABLE team_member (
-    `id` INT(60) PRIMARY KEY AUTO_INCREMENT,
-    `team_id` INT(60),
-    `user_id` INT(60)
 )DEFAULT CHARSET=utf8;
 
 -- question

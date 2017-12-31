247ec475c87211c096ca4072d7aee36dbcc6a4b2
HayatoDoi <b1517914@planet.kanazawa-it.ac.jp>
2017-12-31 20:31:21 +0900

    user session data

--- src/web/models/AnswerModel/AnswerModel.go
+++ src/web/models/AnswerModel/AnswerModel.go
@@ -47,38 +47,37 @@ func (m *AnswerModel) CheckFlag(id int, flag string) (bool, error) {
 	return count > 0, nil
 }
 
-func (m *AnswerModel) IsCorrected(id int, username string) (bool, error) {
+func (m *AnswerModel) IsCorrected(questionId int, teamId interface{}) (bool, error) {
 	m.Open()
 	defer m.Close()
 
 	query := fmt.Sprintf(`
 		SELECT COUNT(*) FROM answer
-		INNER JOIN question ON question.flag = answer.flag
-		INNER JOIN user ON user.id = answer.user_id
-		WHERE answer.question_id = ? AND
-			user.team_id IN (SELECT team_id FROM user WHERE name = ?)
+			INNER JOIN question ON question.flag = answer.flag
+			INNER JOIN user ON user.id = answer.user_id
+			WHERE answer.question_id = ? AND user.team_id = ?
 	`)
 	stmtOut, err := m.Connection.Prepare(query)
 	if err != nil {
 		return false, errors.New("Database query error")
 	}
 	var count int
-	if stmtOut.QueryRow(id, username).Scan(&count) != nil {
+	if stmtOut.QueryRow(questionId, teamId.(string)).Scan(&count) != nil {
 		return false, errors.New("Database error")
 	}
 	return count > 0, nil
 }
 
-func (m *AnswerModel) Insert(QuestionId int, userName string, flag string) error {
+func (m *AnswerModel) Insert(questionId int, userId interface{}, flag string) error {
 	m.Open()
 	defer m.Close()
 
-	query := fmt.Sprintf("INSERT INTO %s (user_id, question_id, flag) SELECT id, ?, ? FROM user WHERE name = ?", m.Table)
+	query := fmt.Sprintf("INSERT INTO %s (user_id, question_id, flag) VALUES( ?, ?, ?)", m.Table)
 	stmtOut, err := m.Connection.Prepare(query)
 	if err != nil {
 		return errors.New("Database : query error")
 	}
-	if stmtOut.QueryRow(QuestionId, flag, userName) == nil {
+	if stmtOut.QueryRow(userId, questionId, flag) == nil {
 		return errors.New("Database error")
 	}
 	return nil
